fbe78ceb154510ae7e74cc28348345052086e3ed
Daniel Mies <daniel.mies@codecentric.de>
2021-08-31 08:38:15 +0200

    di sample

---
+++ di/di.go
@@ -0,0 +1,16 @@
+package di
+
+import (
+	"fmt"
+	"io"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+/*
+func main() {
+	Greet(os.Stdout, "Elodie")
+}
+*/
