3570f008987caa54ffdcbde3f17d7e23829c93d6
Daniel Zenzes <daniel@zenzes.me>
2022-08-31 20:12:15 +0200

    support for name parameter that defaults to World

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-func Hello(name string) string{
-	return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
-func main(){
-	fmt.Println(Hello("world"))
+func main() {
+	fmt.Println(Hello("World"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,11 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
-	got := Hello("Daniel")
-	want := "Hello, Daniel"
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
-	if got != want{
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
